daf63091bd103c5e5576c6c52809cdc9db3136fa
Chase LaRocca <39739602+chase-larocca@users.noreply.github.com>
2021-12-22 22:19:41 -0600

    Add files via upload

---
+++ out/build/x64-Debug/glfw-3.2/cmake_install.cmake
@@ -0,0 +1,40 @@
+# Install script for directory: C:/Users/chase/Desktop/final-project3-team-henchman-2-batman-dark-knight/glfw-3.2
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Users/chase/Desktop/final-project3-team-henchman-2-batman-dark-knight/out/install/x64-Debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/chase/Desktop/final-project3-team-henchman-2-batman-dark-knight/out/build/x64-Debug/glfw-3.2/src/cmake_install.cmake")
+
+endif()
+
